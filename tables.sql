04359e15c03500d428b4048b91fdf52820da9672
Peter Lesslie <pclesslie@gmail.com>
2015-05-11 21:13:32 -0500

    Initial table design

---
+++ tables.sql
@@ -0,0 +1,31 @@
+-- Job Templates
+CREATE TABLE job_template (
+       id SERIAL PRIMARY KEY,
+       template_name VARCHAR NOT NULL,
+       cron_entry VARCHAR NOT NULL,
+       cmd_line VARCHAR NOT NULL,
+       hardware_id INTEGER NOT NULL, -- references hardware(id)
+       writes_sentinels BOOLEAN NOT NULL DEFAULT FALSE
+       );
+
+-- Job Instances
+CREATE TABLE job_instance (
+       id SERIAL PRIMARY KEY,
+       template INTEGER REFERENCES job_template(id), -- null indicates non-template job
+       warnings INTEGER NOT NULL DEFAULT 0,
+       errors INTEGER NOT NULL DEFAULT 0,
+       status VARCHAR NOT NULL DEFAULT 'running', -- { "running", "failed", "finished" }
+       wiki_link VARCHAR NOT NULL DEFAULT '',
+       acknowledged INTEGER, -- REFERENCES users(id)
+       log_file VARCHAR NOT NULL DEFAULT '',
+       start_ts TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+       last_event_ts TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+       last_event VARCHAR NOT NULL DEFAULT '',
+       job_owner INTEGER -- REFERENCES users(id)
+       );
+       
+       
+       
+       
+       
+       	 
